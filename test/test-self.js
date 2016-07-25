530dbb479013fb51b2f4005691724c1ba33fe0ea
Sam Roberts <vieuxtech@gmail.com>
2016-07-25 09:44:48 -0700

    test: update to tap 6.x

--- test/test-self.js
+++ test/test-self.js
@@ -5,18 +5,20 @@
 
 require('./helpers');
 
-var info = require('../package.json');
-var root = path.dirname(require.resolve('../'));
+tap.test('ls self', function(t) {
+  var info = require('../package.json');
+  var root = path.dirname(require.resolve('../'));
 
-console.log('root: %s', root);
+  ls.read(root, function(er, json) {
+    assert.ifError(er);
 
-ls.read(root, function(er, json) {
-  assert.ifError(er);
+    assert.equal(info.version, json.version);
+    assert.equal(info.name, json.name);
+    assert.equal('read-installed', json.dependencies['read-installed'].name);
+    assert(json.dependencies['read-installed']._id);
+    assert(json.dependencies['read-installed']._shasum);
+    assert(json.dependencies['read-installed']._from);
+  });
 
-  assert.equal(info.version, json.version);
-  assert.equal(info.name, json.name);
-  assert.equal('read-installed', json.dependencies['read-installed'].name);
-  assert(json.dependencies['read-installed']._id);
-  assert(json.dependencies['read-installed']._shasum);
-  assert(json.dependencies['read-installed']._from);
+  t.end();
 });
